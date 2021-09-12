499a86d2cd860ce6f9c627f8486932faf1414807
unknown <A01351361@itesm.mx>
2021-09-12 18:08:11 -0500

    Parte 2 Actualizado

--- step2.h
+++ step2.h
@@ -1,32 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void imprimirPalindromo(string transmission, int inicio, int fin){
-  for (int i = inicio; i <= fin; ++i)
-      cout << transmission[i];
-}
-
 int longestPalindrome(string transmission){
     int T = transmission.length();
     int End = 1,start = 0;
-    for (int i = 0; i < T; i++) {
+    int i = 0;
+    while (i < T){
         for (int j = i; j < T; j++) {
-            int flag = 1;
+            int p = 1; //Nuestro flag para el palindromo
 
-            for (int k = 0; k < (j - i + 1) / 2; k++)
-                if (transmission[i + k] != transmission[j - k])
-                    flag = 0;
+            for (int k=0; k < ((j-i+1)/2); k++)
+                if (transmission[i + k] != transmission[j-k])
+                    p = 0;
 
-            if (flag && (j - i + 1) > End) {
+            if (p && (j - i + 1) > End) {
                 start = i;
                 End = j - i + 1;
             }
         }
+        i++;
     }
     int posicionInicial =start;
     int posicionFinal =start + End;
     cout << "Palindromo mas largo en transmission: ";
-    imprimirPalindromo(transmission, posicionInicial, posicionFinal - 1);
+
+    for (int i = posicionInicial; i <= posicionFinal - 1; ++i){
+        cout << transmission[i];
+    }
+
     cout << endl << "Posicion Inicial: " << posicionInicial <<" Posicion Final: " << posicionFinal<< endl;
     return posicionFinal;
 }
