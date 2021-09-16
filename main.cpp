d7aa1d4d4ffb58896a68129e121f9c4f8d3091d1
Pepesitoxd224 <josecachiluis@hotmail.com>
2021-09-16 18:10:39 -0500

    implementacion de la parte 3

--- main.cpp
+++ main.cpp
@@ -1,50 +1,67 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #include "step1.h"
 #include "step2.h"
+
 using namespace std;
 
-int main(){
-    string line, transmission1, transmission2,mcode1, mcode2, mcode3;
+int main() {
+    string line, transmission1, transmission2, mcode1, mcode2, mcode3;
 
     ifstream maliciousCode1("mcode1.txt");
-    getline (maliciousCode1, mcode1);
+    getline(maliciousCode1, mcode1);
     maliciousCode1.close();
 
     ifstream maliciousCode2("mcode2.txt");
-    getline (maliciousCode2, mcode2);
+    getline(maliciousCode2, mcode2);
     maliciousCode2.close();
 
     ifstream maliciousCode3("mcode3.txt");
-    getline (maliciousCode3, mcode3);
+    getline(maliciousCode3, mcode3);
     maliciousCode3.close();
 
     ifstream transmissionCode1("transmission1.txt");
-    while(getline (transmissionCode1, line)){
+    while (getline(transmissionCode1, line)) {
         transmission1 += line;
     };
     transmissionCode1.close();
 
     ifstream transmissionCode2("transmission2.txt");
-    while(getline (transmissionCode2, line)){
+    while (getline(transmissionCode2, line)) {
         transmission2 += line;
     };
     transmissionCode2.close();
 
-    cout << endl << "Parte 1"<< endl;
+    cout << endl << "Parte 1" << endl;
     findMalicious(transmission1, mcode1);
     findMalicious(transmission1, mcode2);
     findMalicious(transmission1, mcode3);
     findMalicious(transmission2, mcode1);
     findMalicious(transmission2, mcode2);
     findMalicious(transmission2, mcode3);
-    cout << endl << endl << "Parte 2"<< endl;
+    cout << endl << endl << "Parte 2" << endl;
 
     findLongestPal(transmission1);
     findLongestPal(transmission2);
 
-    cout << endl << endl << "Parte 3"<< endl;
+    cout << endl << endl << "Parte 3" << endl;
+
+    int P1 = PalMoreLenght(transmission1);
+    int P2 = PalMoreLenght(transmission2);
+
+    if (P1 > P2) {
+
+        cout << "El palindromo mas largo esta en el archivo -transmission1- " << endl;
+        findLongestPal(transmission1);
+    }
+    else {
+
+        cout << "El palindromo mas largo esta en el archivo -transmission2- " << endl;
+        findLongestPal(transmission2);
+    }
+
 
 
     return 0;
--- step2.h
+++ step2.h
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-bool findLongestPal(string transmission){
+bool findLongestPal(string transmission) {
     int T = transmission.length();
-    int fin = 1,inicio = 0;
+    int fin = 1, inicio = 0;
     int i = 0;
-    while (i < T){
+    while (i < T) {
         for (int j = i; j < T; j++) {
             int p = 1;
 
-            for (int k=0; k < ((j-i+1)/2); k++)
-                if (transmission[i + k] != transmission[j-k])
+            for (int k = 0; k < ((j - i + 1) / 2); k++)
+                if (transmission[i + k] != transmission[j - k])
                     p = 0;
 
             if (p && (j - i + 1) > fin) {
@@ -20,14 +20,49 @@ bool findLongestPal(string transmission){
         }
         i++;
     }
-    int posicionInicial =inicio;
-    int posicionFinal =inicio + fin;
+    int posicionInicial = inicio;
+    int posicionFinal = inicio + fin;
     cout << "Palindromo mas largo en transmission: ";
 
-    for (int i = posicionInicial; i <= posicionFinal - 1; ++i){
+    for (int i = posicionInicial; i <= posicionFinal - 1; ++i) {
         cout << transmission[i];
     }
 
-    cout << endl << "Posicion Inicial: " << posicionInicial <<" Posicion Final: " << posicionFinal<< endl;
+    cout << endl << "Posicion Inicial: " << posicionInicial << " Posicion Final: " << posicionFinal << endl;
     return true;
+}
+
+int PalMoreLenght(string transmission) {
+
+    int T = transmission.length();
+    int fin = 1, inicio = 0;
+    int i = 0;
+    while (i < T) {
+        for (int j = i; j < T; j++) {
+            int p = 1;
+
+            for (int k = 0; k < ((j - i + 1) / 2); k++)
+                if (transmission[i + k] != transmission[j - k])
+                    p = 0;
+
+            if (p && (j - i + 1) > fin) {
+                inicio = i;
+                fin = j - i + 1;
+            }
+        }
+        i++;
+    }
+    int posicionInicial = inicio;
+    int posicionFinal = inicio + fin;
+    
+
+    for (int i = posicionInicial; i <= posicionFinal - 1; ++i) {
+        
+        int PML = transmission[i];
+
+        return PML;
+    }
+
+    
+
 }
